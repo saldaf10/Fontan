0a17479c497305119e0d4ab531646582065fff0e
Jose Duque <duquepiedrahitaalejandro@gmail.com>
2024-03-26 00:47:37 -0500

    Actualizacion exitosa de imagenes por parte del usuario

--- Doc_management/views.py
+++ Doc_management/views.py
@@ -84,29 +84,35 @@ def delete_record(request, pk):
 
 
 def add_record(request):
-	form = AddRecordForm(request.POST or None)
-	if request.user.is_authenticated:
-		if request.method == "POST":
-			if form.is_valid():
-				add_record = form.save()
-				messages.success(request, "Record Added...")
-				return redirect('home')
-		return render(request, 'add_record.html', {'form':form})
-	else:
-		messages.success(request, "You Must Be Logged In...")
-		return redirect('home')
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            form = AddRecordForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Added...")
+                return redirect('home')
+        else:
+            form = AddRecordForm()
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
+
 
 
 def update_record(request, pk):
-	if request.user.is_authenticated:
-		current_record = Estudiante.objects.get(id=pk)
-		form = AddRecordForm(request.POST or None, instance=current_record)
-		if form.is_valid():
-			form.save()
-			messages.success(request, "Record Has Been Updated!")
-			return redirect('home')
-		return render(request, 'update_record.html', {'form':form})
-	else:
-		messages.success(request, "You Must Be Logged In...")
-		return redirect('home')
+    if request.user.is_authenticated:
+        current_record = Estudiante.objects.get(id=pk)
+        if request.method == "POST":
+            form = AddRecordForm(request.POST, request.FILES, instance=current_record)
+            if form.is_valid():
+                form.save() 
+                messages.success(request, "Record Has Been Updated!")
+                return redirect('home')
+        else:
+            form = AddRecordForm(instance=current_record)
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
 
